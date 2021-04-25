2f7147243c94ff7e8530d796d58baad4fadf2ff8
zulhusni hariz <zulhusnihariz97@gmail.com>
2021-04-26 02:37:43 +0800

    added filter features and clear completed button

--- app.js
+++ app.js
@@ -3,32 +3,26 @@ var app = new Vue({
   data() {
     return {
       newItem: "",
-      editedItem: "",
+      currentItem: "",
       todoList: [],
-      checkList: [],
+      currentList: [],
     };
   },
 
   mounted() {
     if (localStorage.todoList) {
       this.todoList = JSON.parse(localStorage.todoList);
+      this.currentList = this.todoList;
     }
   },
 
   watch: {
     todoList: {
       handler(newList) {
-        console.log("localStorage updated");
         localStorage.todoList = JSON.stringify(newList);
       },
 
       deep: true,
-    },
-
-    checkList: {
-      handler(newList) {
-        localStorage.checkList = JSON.stringify(newList);
-      },
     },
   },
 
@@ -36,44 +30,67 @@ var app = new Vue({
     addItems() {
       if (this.newItem === "") {
       } else {
-        console.log("Enter Key pressed. New item added");
         this.todoList.push({
           todoItem: this.newItem,
           isChecked: false,
         });
-
         this.newItem = "";
-
-        this.checklistUpdate;
       }
     },
 
-    toggleChecked(item, index) {
-      let selectedItem = this.todoList[index].todoItem;
-      let itemExists = this.checkList.includes(selectedItem);
-
+    toggleChecked(item) {
       item.isChecked = !item.isChecked;
     },
 
-    editItem(index) {
+    editItem(item, index) {
       let edited = this.$refs["editField"][index].innerHTML;
-      this.todoList[index].todoItem = edited;
-      console.log("Value in localStorage updated");
+      console.log(item);
+
+      this.todoList[this.todoList.indexOf(item)].todoItem = edited;
     },
 
-    removeItem(index) {
-      console.log("Delete button clicked");
+    removeItem(item) {
+      if (this.currentList == this.activeList) {
+        this.todoList.splice(this.todoList.indexOf(item), 1);
+        this.currentList = this.activeList;
+      } else if (this.currentList == this.completedList) {
+        this.todoList.splice(this.todoList.indexOf(item), 1);
+        this.currentList = this.completedList;
+      } else {
+        this.todoList.splice(this.todoList.indexOf(item), 1);
+      }
+    },
 
-      console.log(this.todoList.splice(index, 1)); // why this line work?
+    removeAll() {
+      let i = this.todoList.length - 1;
 
-      console.log("Item removed");
+      // this.completedList.length = 0;
+
+      while (i != -1) {
+        if (this.todoList[i].isChecked) {
+          this.removeItem(this.todoList[i]);
+          i--;
+        } else {
+          i--;
+        }
+      }
     },
   },
+
   computed: {
-    filteredList: function () {
-      return this.todoList.filter((item) => {
+    activeList: function () {
+      activeList = this.todoList.filter((item) => {
         return !item.isChecked;
       });
+
+      return activeList;
+    },
+    completedList: function () {
+      completedList = this.todoList.filter((item) => {
+        return item.isChecked;
+      });
+
+      return completedList;
     },
   },
 });
